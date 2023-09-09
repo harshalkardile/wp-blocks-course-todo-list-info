3b26f951bcccce03392fca5c849d453c881aeece
harshal <kardileharshal567@gmail.com>
2023-09-09 18:37:31 +0530

    completed the todolist info part

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'fe0de1ecc4cbf6060c6b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd74da8199a9db3c92522');
